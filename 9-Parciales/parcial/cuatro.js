f85da71f915a5eee98d2a4d51c79b4d7f3a2b64c
Nahuee <36265001+nahuarceluz@users.noreply.github.com>
2018-02-15 12:18:28 -0300

    Clase 3 (15-2-18)
     If, else..

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,28 @@
 function Mostrar()
 {
+    var num1;
+    var num2;
+    var resultado;
 
+    num1= prompt ("Ingrese número");
+    num2= prompt ("Ingrese número");
+
+    num1= parseInt (num1);
+    num2= parseInt (num2);
+
+    if(num1==num2)
+    {
+        resultado= num1*num2;
+    }
+    else if(num1>num2)
+    {
+        resultado= num1-num2;
+    }
+    else
+    {
+        resultado= num1+num2;
+    }
+    
+
+    document.write (resultado);
 }
