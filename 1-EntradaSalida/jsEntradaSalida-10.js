6a578a0462b28069f0aa22978bf1bcd8464e1f2a
Nahuee <36265001+nahuarceluz@users.noreply.github.com>
2018-02-16 20:27:18 -0300

    Clases 8/2 y 9/2
    importe con descuento (%)..

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,17 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+    var descuento;
+    var resultado;
+
+    importe= document.getElementById("importe").value;
+    descuento= document.getElementById("resultado").value;
+
+    importe= parseInt (importe);
+    descuento= importe*0.25;
+
+    resultado= importe+descuento
+
+    alert (resultado);
 }
